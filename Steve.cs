7d377e51913def4e85b12e064fb8d6b3d338ad60
AlbertSpichkin <116497161+AlbertSpichkin@users.noreply.github.com>
2022-10-23 17:24:26 +0300

    steven
    stevenlol

---
+++ Steve.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steve : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 3, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 3, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 4, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
